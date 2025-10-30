d96504d93de66dc435a1d3522c6a1714423809d0
Wells-Wait <wells.wait.coder@gmail.com>
2025-10-30 08:42:54 -0600

    Initial Commit

---
+++ RootedTogether/RootedTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootedTogetherApp.swift
+//  RootedTogether
+//
+//  Created by Wells Wait on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct RootedTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RootedTogetherTests/RootedTogetherTests.swift
@@ -0,0 +1,17 @@
+//
+//  RootedTogetherTests.swift
+//  RootedTogetherTests
+//
+//  Created by Wells Wait on 10/30/25.
+//
+
+import Testing
+@testable import RootedTogether
+
+struct RootedTogetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
